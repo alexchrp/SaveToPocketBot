0233529760dffa2ae0d4975a49a60aeda73e88a9
alexchrp <illest.gt@gmail.com>
2019-10-20 14:04:00 +0500

    add connection to dynamodb

--- savetopocketbot.go
+++ savetopocketbot.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"log"
 	"net/http"
 	"os"
@@ -12,13 +16,12 @@ import (
 )
 
 var (
-	URL             = "https://api.telegram.org/bot" + os.Getenv("TOKEN") + "/"
-	ResponseHeaders = map[string]string{"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"}
+	tgUrl            = "https://api.telegram.org/bot" + os.Getenv("TOKEN") + "/"
+	responseHeaders  = map[string]string{"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"}
+	ss, _            = session.NewSession(aws.NewConfig().WithRegion("eu-central-1"))
+	db               = dynamodb.New(ss)
+	pocketTokenTable = os.Getenv("POCKET_TOKEN_TABLE")
 )
-
-type ReceivedEvent struct {
-	Body string `json:"body"`
-}
 
 type Body struct {
 	Message Message `json:"message"`
@@ -48,25 +51,59 @@ type Chat struct {
 
 func sendMessage(text, chatId string) {
 	finalText := "You said: " + text
-	url := fmt.Sprintf("%ssendMessage?text=%s&chat_id=%s", URL, finalText, chatId)
+	url := fmt.Sprintf("%ssendMessage?text=%s&chat_id=%s", tgUrl, finalText, chatId)
 	result, _ := http.Get(url)
 	log.Println("Send message result: ", result)
 }
 
-func parseMessage(receivedEvent ReceivedEvent) Message {
+func parseMessage(body string) Message {
 	var event Body
-	err := json.Unmarshal([]byte(receivedEvent.Body), &event)
+	err := json.Unmarshal([]byte(body), &event)
 	if err != nil {
 		log.Printf("err was %v", err)
 	}
 	return event.Message
 }
 
-func HandleRequest(receivedEvent ReceivedEvent) (events.APIGatewayProxyResponse, error) {
-	message := parseMessage(receivedEvent)
+func HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	message := parseMessage(request.Body)
+	processMessage(message)
+	return events.APIGatewayProxyResponse{StatusCode: 200, Headers: responseHeaders}, nil
+}
+
+func processMessage(message Message) {
 	chatId := message.Chat.Id
+	if message.Text == "/start" {
+		createPocketApiToken(message.User)
+	}
 	sendMessage(message.Text, strconv.Itoa(chatId))
-	return events.APIGatewayProxyResponse{StatusCode: 200, Headers: ResponseHeaders}, nil
+}
+
+type UserToken struct {
+	Id    string `json:"id"`
+	Token string `json:"token"`
+}
+
+func createPocketApiToken(user User) {
+	token, err := createToken(user)
+	if err != nil {
+
+	}
+	item := UserToken{strconv.Itoa(user.Id), token}
+	av, err := dynamodbattribute.MarshalMap(item)
+	input := &dynamodb.PutItemInput{
+		TableName: aws.String(pocketTokenTable),
+		Item:      av,
+	}
+	_, err = db.PutItem(input)
+	if err != nil {
+		fmt.Println("Got error calling PutItem:")
+		fmt.Println(err.Error())
+	}
+}
+
+func createToken(user User) (string, error) {
+	return "testToken", nil
 }
 
 func main() {
